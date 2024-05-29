aaa4c08921b14cbd38414bc604c819302b65cdc7
bartlomiej-jedrol <bartlomiej.jedrol@gmail.com>
2024-05-29 13:00:09 +0200

    modified

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,14 +14,40 @@ terraform {
   }
 }
 
+# S3 bucket raw data
 resource "aws_s3_bucket" "raw_bucket" {
   bucket = var.raw_bucket
 }
 
+
+# S3 bucket lakehouse
+resource "aws_s3_bucket" "lakehouse_bucket" {
+  bucket = var.lakehouse_bucket
+}
+
+resource "aws_s3_object" "bronze_folder" {
+  bucket = aws_s3_bucket.lakehouse_bucket.id
+  key    = "lakehouse/bronze"
+}
+
+resource "aws_s3_object" "silver_folder" {
+  bucket = aws_s3_bucket.lakehouse_bucket.id
+  key    = "lakehouse/silver"
+}
+
+resource "aws_s3_object" "gold_folder" {
+  bucket = aws_s3_bucket.lakehouse_bucket.id
+  key    = "lakehouse/gold"
+}
+
+
+# ECR lambda repo
 resource "aws_ecr_repository" "lambda_repo" {
   name = var.lambda_repo
 }
 
+
+# Lambda
 resource "aws_iam_role" "lambda_role" {
   name = var.lambda_role
   assume_role_policy = jsonencode({
@@ -40,7 +66,7 @@ resource "aws_iam_role" "lambda_role" {
 
 resource "aws_lambda_function" "lambda" {
   function_name = var.lambda
-  timeout       = 10
+  timeout       = 30
   image_uri     = "${aws_ecr_repository.lambda_repo.repository_url}:latest"
   package_type  = "Image"
 
@@ -53,9 +79,11 @@ resource "aws_lambda_function" "lambda" {
   }
 }
 
+
+# CloudWatch event
 resource "aws_cloudwatch_event_rule" "event" {
   name                = var.lambda_event_rule
-  schedule_expression = "cron(0 6 * * ? *)"
+  schedule_expression = "rate(2 hours)"
 }
 
 resource "aws_cloudwatch_event_target" "event_target" {
@@ -63,10 +91,51 @@ resource "aws_cloudwatch_event_target" "event_target" {
   arn  = aws_lambda_function.lambda.arn
 }
 
+
+# Lambda permission for CloudWatch events
 resource "aws_lambda_permission" "allow_cloudwatch" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda.function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.event.arn
+}
+
+
+# Glue
+resource "aws_iam_role" "glue_role" {
+  name = var.glue_role
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "glue.amazonaws.com"
+      }
+    }]
+  })
+
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonS3FullAccess", "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"]
+}
+
+# S3 bucket for glue script
+resource "aws_s3_bucket" "glue_source_bucket" {
+  bucket = var.glue_source_bucket
+}
+
+# Glue databases for the lakehouse
+resource "aws_glue_catalog_database" "bronze_database" {
+  name         = var.glue_bronze_database
+  location_uri = "s3://de03-lakehouse/lakehouse/bronze"
+}
+
+resource "aws_glue_catalog_database" "silver_database" {
+  name         = var.glue_silver_database
+  location_uri = "s3://de03-lakehouse/lakehouse/silver"
+}
+
+resource "aws_glue_catalog_database" "gold_database" {
+  name         = var.glue_gold_database
+  location_uri = "s3://de03-lakehouse/lakehouse/gold"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,6 +3,11 @@ variable "raw_bucket" {
   default = "de03-raw-data"
 }
 
+variable "lakehouse_bucket" {
+  type    = string
+  default = "de03-lakehouse"
+}
+
 variable "lambda_repo" {
   type    = string
   default = "de03-lambda-repo"
@@ -22,3 +27,46 @@ variable "lambda_event_rule" {
   type    = string
   default = "de03-trigger-lambda"
 }
+
+variable "glue_role" {
+  type    = string
+  default = "de03-glue-role"
+}
+
+variable "glue_source_bucket" {
+  type    = string
+  default = "de03-glue-source"
+}
+
+variable "glue_bronze_database" {
+  type    = string
+  default = "de03-bronze"
+}
+
+variable "glue_silver_database" {
+  type    = string
+  default = "de03-silver"
+}
+
+variable "glue_gold_database" {
+  type    = string
+  default = "de03-gold"
+}
+
+variable "s3_location_bronze_database" {
+  type    = string
+  default = "de03-gold"
+}
+
+variable "s3_location_silver_database" {
+  type    = string
+  default = "de03-gold"
+}
+
+variable "s3_location_gold_database" {
+  type    = string
+  default = "de03-gold"
+}
+
+
+
